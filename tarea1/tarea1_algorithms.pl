da717735a94dd820912fa093a692380d3cf19153
cletsxdv <jclementehdzhdz@gmail.com>
2019-10-06 14:54:23 -0500

    Agregado de sumaPeano/3

--- tarea1/tarea1_algorithms.pl
+++ tarea1/tarea1_algorithms.pl
@@ -3,14 +3,31 @@
 peanoNat(Sec, N) :-
     is_nat(Sec, N).
 
+/*sumaPeano_PersonalVersion(Sec1, Sec2, R) :-
+    is_nat(Sec1, NumSec1),
+    is_nat(Sec2, NumSec2),
+    R is NumSec1 + NumSec2.*/
+
+%%% sumaPeano/3. sumaPeano(+Secuencia1, +Secuencia2, -Resultado).
+% Resultado es la suma de Peano de Secuencia1 y Secuencia2.
+% Caso 1: base. Cuando la Sencuencia2 llega a 0, se termina el ciclo recursivo causado por Caso 1 y 2.
+sumaPeano(s(0), 0, s(0)).
+% Caso 2: recursivo. Si la Secuencia1 llega a s(0), comienza a agregar la Secuencia2 a Resultado.
+    % Secuencia1 solo llega a s(0) simplemente para que no se agregue un 0 y eso haga terminar el ciclo.
+sumaPeano(s(0), s(X2), s(R)) :-
+    sumaPeano(s(0), X2, R).
+% Caso 3: recursivo. Cuando las dos secuencias contienen términos en sí mismas.
+sumaPeano(s(X1), Sec2, s(R)) :-
+    sumaPeano(X1, Sec2, R).
+
 %%% is_nat/2. is_nat(+NumInSec, -N).
 % Si NumInSec (número en la Secuencia) es un número natural, N es número de Peano.
-% Basada en:
+% Basado en:
     % 1. http://www.lcc.uma.es/~lopez/progdec/prolog/apuntes/02-basicas/tecnicas_basicas.pdf (páginas 1 - 7)
     % 2. https://www.uv.mx/personal/aguerra/files/2019/08/pia-slides-02.pdf (slide 45)
     % 3. http://enciclopedia.us.es/index.php/Axiomas_de_Peano
 % N es el número de Peano, el cual se computa dentro de esta función.
-% Cuando la función unifica a 0 con NumInSec (Caso 1), 
+% Cuando la función unifica a 0 con NumInSec (Caso 1),
     % N también unifica con 0, y computa (en Caso 2).
 % Caso 1: base.
 is_nat(0, 0).
@@ -35,11 +52,11 @@ subset([Element | Rest], SetB) :-
 % El ConjuntoR es la intersección del ConjuntoA con el ConjuntoB.
 % Caso 1: base.
 inter([], _, []).
-% Caso 2: recursivo. Cuando el Elemento no forma parte de Set.
+% Caso 2: recursivo. Cuando el Elemento no forma parte de SetB.
 inter([Element | Rest], SetB, List) :-
     \+ member(Element, SetB),
     inter(Rest, SetB, List).
-% Caso 3: recursivo. Cuando el Elemento se encuentra en Set.
+% Caso 3: recursivo. Cuando el Elemento se encuentra en SetB.
 inter([Element | Rest], SetB, [Element | List]) :-
     member(Element, SetB),
     inter(Rest, SetB, List).
